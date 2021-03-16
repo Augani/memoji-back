3ed080abc3ab66b1012b89585d65fb87ca1fece1
Augustus Otu <Augani7@gmail.com>
2021-03-16 21:06:17 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const bodyParser = require("body-parser");
+var express = require("express");
+var Pair = require("./pair");
+var app = express();
+app.use(bodyParser.json());
+console.log(Pair["bm"]);
+let URL = (n) =>
+  `https://res.cloudinary.com/augani/image/upload/v1615924685/Memoji/Memoji-${n}.png`;
+function List() {
+  let data = [];
+  let y = [...Pair["bm"], ...Pair["wf"], ...Pair["wm"]];
+  y.map((m) => {
+    data.push(URL(m));
+  });
+  return JSON.stringify(data);
+}
+
+function random(gen = null, skin = null, all) {
+  if (all != 1) return List();
+  let p =
+    gen === "b" && skin === "m"
+      ? Pair["bm"]
+      : gen === "f" && skin === "w"
+      ? Pair["wf"]
+      : gen === "m" && skin === "w"
+      ? Pair["wm"]
+      : [...Pair["bm"], ...Pair["wf"], ...Pair["wm"]];
+  let b = Math.floor(Math.random() * Math.floor(p.length));
+  return URL(p[b]);
+}
+
+app.get("/v1/:n?/:g?/:s?", function (req, res) {
+  let q = req.params.n || 1;
+  let gender = req.params.g || null;
+  let skin = req.params.s || null;
+  res.status(200).send(random(gender, skin, q));
+});
+
+app.listen(process.env.port || 4000);
